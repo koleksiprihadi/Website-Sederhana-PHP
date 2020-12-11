6486ccec2a4c90f0200d6143af28a104ceed543e
koleksiprihadi <krisna.prihadiyanto@gmail.com>
2020-12-11 09:32:39 +0700

    progress 2

---
+++ kursus.php
@@ -0,0 +1,63 @@
+<?php require_once("koneksi/dbkoneksi.php")?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <?php require_once("header.php") ?>
+</head>
+<body>
+    <div class="row">
+        <div class="col-9">
+            <div class="container">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="<?php echo $_GET["id_kursus"];?>" title="asdfg" allowfullscreen></iframe>
+                </div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="detail-tab" data-toggle="tab" href="#detail" role="tab" aria-controls="detail" aria-selected="true">Detail kursus</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="qna-tab" data-toggle="tab" href="#qna" role="tab" aria-controls="qna" aria-selected="false">QnA</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pengumuman-tab" data-toggle="tab" href="#pengumuman" role="tab" aria-controls="pengumuman" aria-selected="false">Pengumuman</a>
+                </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="detail" role="tabpanel" aria-labelledby="detail-tab">A</div>
+                <div class="tab-pane fade" id="qna" role="tabpanel" aria-labelledby="qna-tab">B</div>
+                <div class="tab-pane fade" id="pengumuman" role="tabpanel" aria-labelledby="pengumuman-tab">C</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="">
+                <?php 
+                $id_qursus = 1;
+                $query = mysqli_query($con,"SELECT * FROM video WHERE id_kursus= $id_qursus ");
+                while ($record = mysqli_fetch_array($query)) { ?>
+                    <div class="kursus">
+                        <form method="get">
+                        <input type="text" name="id_kursus" value="<?php echo $record['link']; ?>"hidden>
+                            <input type="submit" name="id" value="<?php echo $record['nama']; ?>" class="btn">
+                        </form>
+                    </div>
+                <?php } ?>
+            
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+    // $query = mysqli_query($con,"SELECT * FROM video WHERE nama LIKE "%CSS%"");
+    // if(isset($_POST['submit'])){
+    //     $name1 = $_GET["name"];
+    //     $play = mysqli_query($con,"SELECT * FROM video WHERE nama LIKE "%$name1%"");
+    //     $record_play = mysqli_fetch_array($play);
+    // }
+    
+?>
---
+++ pretier.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+          <!-- Css -->
+          <link rel="stylesheet" href="assets/style.css">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+          <!-- Js -->
+          <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">MENU</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ONLINE COURSE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CHALLANGE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">BLOG</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">PHP</a>
+            <a class="dropdown-item" href="#">HTML</a>
+            <a class="dropdown-item" href="#">CSS</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+
+  <div class="card-group">
+    <div class="card">
+      <img src="PWL/FP/komp.jpg" class="card-img-top" alt="browse">
+      <div class="card-body">
+        <h5 class="card-title">HTML NIH GAIS</h5>
+        <p class="card-text">JADI GAIS,INI UNTUK HTML </p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
